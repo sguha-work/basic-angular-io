d9c571e686dbf4aa813a37cdc1b3c0afc0dbd528
sguha-work <sguha1988.life@gmail.com>
2017-07-18 00:11:50 +0530

    jquery example

--- login-signup/src/app/components/login/login.component.ts
+++ login-signup/src/app/components/login/login.component.ts
@@ -1,19 +1,29 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import {AfterViewInit} from '@angular/core';
 import {UserService} from './../../services/user.service';
+declare var jQuery: any;
 @Component({
   selector: 'login',
   templateUrl: 'login.template.html',
   //styleUrls: ['./app.component.css']
 })
-export class LoginComponent {
+export class LoginComponent  implements AfterViewInit{
   
   private userService: UserService;
   public model: any;
 
+  ngAfterViewInit() {
+        jQuery("input").css({
+            "border": "1px solid red"
+        });
+        alert(jQuery("input").length);
+    }
+
   constructor(private route: ActivatedRoute, private router: Router) {
     this.userService = new UserService();
     this.model = {};
+    
   }
 
   login() {
