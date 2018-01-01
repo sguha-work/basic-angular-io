b92cfeb9f2548f87fa215b29187e8a20be7971b5
sguha-work <sguha1988.life@gmail.com>
2018-01-01 12:24:55 +0530

    testapp

---
+++ TestApp-tdd/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from '../pages/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [HomeComponent]
+})
+export class AppModule { }
